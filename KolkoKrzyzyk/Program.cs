a0f8fedac9e205493b93bc8ccfe030a302f33843
jakub.dedio <jakub.dedio@gmail.com>
2019-04-14 15:07:26 +0200

    Hello GitHub

--- KolkoKrzyzyk/Program.cs
+++ KolkoKrzyzyk/Program.cs
@@ -8,8 +8,39 @@ namespace KolkoKrzyzyk
 {
     class Program
     {
+        static char[,] grid = new char[3, 3] { { '_', '_', '_' }, { '_', '_', '_' }, { '_', '_', '_' } };
         static void Main(string[] args)
         {
+            bool GameRunning = true;
+            
+
+            while(GameRunning)
+            {
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+        static void printGrid()
+        {
+            for(int i = 0; i < 3; ++i)
+            {
+                Console.WriteLine($"{ }")
+                //for(int j = 0; j < 3; j++)
+                //{
+
+               // }
+
+            }
         }
     }
 }
